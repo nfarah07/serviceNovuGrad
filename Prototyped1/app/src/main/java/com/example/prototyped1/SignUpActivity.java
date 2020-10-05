3b1c8d928ade3800b58918d62d24eecdefc91ec7
12DerekH <55858151+12DerekH@users.noreply.github.com>
2020-10-05 17:52:39 -0400

    Fixed some issues

--- Prototyped1/app/src/main/java/com/example/prototyped1/SignUpActivity.java
+++ Prototyped1/app/src/main/java/com/example/prototyped1/SignUpActivity.java
@@ -1,8 +1,5 @@
 package com.example.prototyped1;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -13,13 +10,15 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.prototyped1.account.Customer;
 import com.example.prototyped1.account.Employee;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-//import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
